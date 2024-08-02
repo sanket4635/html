f9b04f9367101336433a7170224764819867bcb6
sanket4635 <sankupol1999@gmail.com>
2024-08-02 14:18:50 +0530

    hellow sanket

--- myjava.java
+++ myjava.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, welcome to fortune cloud!"); 
+        System.out.println("Hello, rushi!"); 
     }
 }

71e264bf3ecc364a6f4312d6abfffe7c322d20ec
sanket4635 <sankupol1999@gmail.com>
2024-08-02 15:00:10 +0530

    rushi

--- myjava.java
+++ myjava.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, rushi!"); 
-    }
+        System.out.println("Hello, rushi welcome!"); 
+ }
 }
